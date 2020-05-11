84cd5f90f50cc758342a786083e32f483e24e224
danny-gonz <dpg2012@hotmail.com>
2020-05-11 16:10:58 -0700

    Updates

---
+++ precache-manifest.93b5a8ad5cb940e97b2a668ea3251ad9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3197e4c823a2978cd9cdc592a6b36c4",
+    "url": "/awesome2day/index.html"
+  },
+  {
+    "revision": "66330c0e06eb4954b636",
+    "url": "/awesome2day/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f1d90dbeb7277d84689",
+    "url": "/awesome2day/static/js/2.4fa4cdfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/awesome2day/static/js/2.4fa4cdfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66330c0e06eb4954b636",
+    "url": "/awesome2day/static/js/main.5680aac8.chunk.js"
+  },
+  {
+    "revision": "b00232e47ed9f2965e2d",
+    "url": "/awesome2day/static/js/runtime-main.0e411783.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/awesome2day/static/media/logo.5d5d9eef.svg"
+  }
+]);
